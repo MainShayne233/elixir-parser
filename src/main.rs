bae955c89d2c2fb08cb35474ba109f40f267b621
MainShayne233 <shaynetremblay@gmail.com>
2019-08-09 09:18:14 -0400

    Parse atoms and strings!

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,35 @@ lalrpop_mod!(pub grammar);
 pub mod ast;
 
 #[test]
-fn test_atom() {
+fn test_atom_colon() {
     let program = grammar::LiteralParser::new()
         .parse(":atom")
         .unwrap();
     assert_eq!(&format!("{:?}", program), "Atom(\"atom\")");
+}
+
+#[test]
+fn test_atom_mixed_case_colon() {
+    let program = grammar::LiteralParser::new()
+        .parse(":AtOm")
+        .unwrap();
+    assert_eq!(&format!("{:?}", program), "Atom(\"AtOm\")");
+}
+
+#[test]
+fn test_atom_with_spaces_colon() {
+    let program = grammar::LiteralParser::new()
+        .parse(":\"i am an atom\"")
+        .unwrap();
+    assert_eq!(&format!("{:?}", program), "Atom(\"i am an atom\")");
+}
+
+#[test]
+fn test_atom_literal() {
+    let program = grammar::LiteralParser::new()
+        .parse("defmodule")
+        .unwrap();
+    assert_eq!(&format!("{:?}", program), "Atom(\"defmodule\")");
 }
 
 #[test]
