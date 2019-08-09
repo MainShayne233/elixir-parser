ea5160c789e6a904c07220554bc6427f35b0dc61
MainShayne233 <shaynetremblay@gmail.com>
2019-08-09 11:12:32 -0400

    Steal EXPR types from gleam!

--- src/ast.rs
+++ src/ast.rs
@@ -1,9 +1,20 @@
-#[derive(Debug)]
-pub enum Primative {
-    Boolean(String),
-    Integer(String),
-    Float(String),
-    Atom(String),
-    String(String),
-}
+pub type UntypedExpr = Expr;
 
+#[derive(Debug, PartialEq, Clone)]
+pub enum Expr {
+    Int {
+        value: i64,
+    },
+
+    Float {
+        value: f64,
+    },
+
+    String {
+        value: String,
+    },
+
+    Atom {
+        value: String,
+    },
+}
--- src/main.rs
+++ src/main.rs
@@ -6,82 +6,80 @@ pub mod ast;
 
 #[test]
 fn test_boolean_true() {
-    let program = grammar::PrimativeParser::new()
+    let program = grammar::UntypedExprParser::new()
         .parse("true")
         .unwrap();
-    assert_eq!(&format!("{:?}", program), "Boolean(\"true\")");
+    assert_eq!(&format!("{:?}", program), "Atom { value: \"true\" }");
 }
 
 #[test]
 fn test_boolean_false() {
-    let program = grammar::PrimativeParser::new()
+    let program = grammar::UntypedExprParser::new()
         .parse("false")
         .unwrap();
-    assert_eq!(&format!("{:?}", program), "Boolean(\"false\")");
+    assert_eq!(&format!("{:?}", program), "Atom { value: \"false\" }");
 }
 
 #[test]
 fn test_integer() {
-    let program = grammar::PrimativeParser::new()
+    let program = grammar::UntypedExprParser::new()
         .parse("124")
         .unwrap();
-    assert_eq!(&format!("{:?}", program), "Integer(\"124\")");
+    assert_eq!(&format!("{:?}", program), "Int { value: 124 }");
 }
 
 #[test]
 fn test_integer_underscore() {
-    let program = grammar::PrimativeParser::new()
+    let program = grammar::UntypedExprParser::new()
         .parse("123_456")
         .unwrap();
-    assert_eq!(&format!("{:?}", program), "Integer(\"123_456\")");
+    assert_eq!(&format!("{:?}", program), "Int { value: 123456 }");
 }
 
 #[test]
 fn test_float() {
-    let program = grammar::PrimativeParser::new()
+    let program = grammar::UntypedExprParser::new()
         .parse("124.123")
         .unwrap();
-    assert_eq!(&format!("{:?}", program), "Float(\"124.123\")");
+    assert_eq!(&format!("{:?}", program), "Float { value: 124.123 }");
 }
-
-
 
 #[test]
 fn test_atom_colon() {
-    let program = grammar::PrimativeParser::new()
+    let program = grammar::UntypedExprParser::new()
         .parse(":atom")
         .unwrap();
-    assert_eq!(&format!("{:?}", program), "Atom(\"atom\")");
+    assert_eq!(&format!("{:?}", program), "Atom { value: \"atom\" }");
 }
 
 #[test]
 fn test_atom_mixed_case_colon() {
-    let program = grammar::PrimativeParser::new()
+    let program = grammar::UntypedExprParser::new()
         .parse(":AtOm")
         .unwrap();
-    assert_eq!(&format!("{:?}", program), "Atom(\"AtOm\")");
+    assert_eq!(&format!("{:?}", program), "Atom { value: \"AtOm\" }");
 }
 
 #[test]
 fn test_atom_with_spaces_colon() {
-    let program = grammar::PrimativeParser::new()
+    let program = grammar::UntypedExprParser::new()
         .parse(":\"i am an atom\"")
         .unwrap();
-    assert_eq!(&format!("{:?}", program), "Atom(\"i am an atom\")");
+    assert_eq!(&format!("{:?}", program), "Atom { value: \"i am an atom\" }");
 }
 
 #[test]
 fn test_atom_literal() {
-    let program = grammar::PrimativeParser::new()
+    let program = grammar::UntypedExprParser::new()
         .parse("defmodule")
         .unwrap();
-    assert_eq!(&format!("{:?}", program), "Atom(\"defmodule\")");
+    assert_eq!(&format!("{:?}", program), "Atom { value: \"defmodule\" }");
 }
 
 #[test]
 fn test_string() {
-    let program = grammar::PrimativeParser::new()
+    let program = grammar::UntypedExprParser::new()
         .parse("\"Hello, world!\"")
         .unwrap();
-    assert_eq!(&format!("{:?}", program), "String(\"Hello, world!\")");
+    assert_eq!(&format!("{:?}", program), "String { value: \"Hello, world!\" }");
 }
